e91aabc20f2088b21d997df6736b838fdbc14d24
ACaspersz <a.casperszz@gmail.com>
2020-11-14 21:35:04 +1100

    views work and controller config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,10 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:phone_number])
     end
+
+    private
+ 
+    def after_sign_in_path_for(resource)
+       "/causes/index"
+    end
 end
---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,16 @@
+class RegistrationsController < Devise::RegistrationsController
+    private
+ 
+    # Modified Devise params for user login
+    def sign_up_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+
+    def after_sign_up_path_for(resource)
+        "/causes/index"
+      end
+   
+      def after_inactive_sign_up_path_for(resource)
+        "/home/index"
+      end
+end
