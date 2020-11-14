d43726c10fc3cc415428cb1e109d05f6c118cb79
ACaspersz <a.casperszz@gmail.com>
2020-11-14 17:39:23 +1100

    generating views, fixing models

--- app/models/cause.rb
+++ app/models/cause.rb
@@ -1,3 +1,4 @@
 class Cause < ApplicationRecord
     belongs_to :user
+    has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :comments, as: :commentable
-  has_one :cause
+  has_many :comments
+  has_many :causes
   has_many :donations
 end
