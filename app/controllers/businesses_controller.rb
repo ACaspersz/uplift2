6602c9b4f1aa4beccce1f47bea31f8c0081a1f5e
ACaspersz <a.casperszz@gmail.com>
2020-11-15 22:51:32 +1100

    installed AWS S3 and configured, removed rich text

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -8,16 +8,34 @@ class BusinessesController < ApplicationController
     end
 
     def create
-    @business.user_id = current_user.id
-    @business.save
+        def create
+            @business = Business.new(business_params)
+        
+            respond_to do |format|
+              if @business.save
+                format.html { redirect_to @business, notice: 'Business was successfully created.' }
+                format.json { render :show, status: :created, location: @business }
+              else
+                format.html { render :new }
+                format.json { render json: @business.errors, status: :unprocessable_entity }
+              end
+            end
+          end
     end
 
     def show
+        @business = Business.all(@user)
     end
 
     def destroy
     end
 
     def update 
+    end
+
+    private
+
+    def business_params
+        params.require(:business).permit(:business_name, :region, :category, :thumbnail, :description)
     end
 end
--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,6 @@
 class Business < ApplicationRecord
     belongs_to :user
     has_many :causes
+    has_many_attached :thumbnails
+    
 end
